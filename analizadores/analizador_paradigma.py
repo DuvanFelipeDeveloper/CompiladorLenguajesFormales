a7cbd0ee2d1c741ab1ad90ffe8465f810290d20f
Michael Gonzalez <michael.gonzalez02@uptc.edu.co>
2023-09-17 17:55:55 -0500

    Update analizador_paradigma.py
    Se creo el analizador de Paradigma

--- analizadores/analizador_paradigma.py
+++ analizadores/analizador_paradigma.py
@@ -1,113 +1,94 @@
 from pygments.lexers import guess_lexer
-from pygments.lexers import get_lexer_for_filename, get_lexer_by_name
-from pygments.util import ClassNotFound
+import ahocorasick
 
 def detect_paradigm(code):
-    try:
-        # Intenta determinar el lenguaje del código
-        lexer = get_lexer_for_filename("example.py", code)
-    except ClassNotFound:
-        # Si no se puede determinar automáticamente, usa un lexer de Python
-        lexer = get_lexer_by_name("python")
-
-    # Detección de paradigmas basada en el lexer
-    detected_paradigms = set()
-
-    if "oop" in lexer.aliases or "Python" in lexer.name:
-        detected_paradigms.add("Programación Orientada a Objetos")
-
-    if "functional" in lexer.aliases:
-        detected_paradigms.add("Programación Funcional")
-
-    if "imperative" in lexer.aliases:
-        detected_paradigms.add("Programación Imperativa")
-
-    return detected_paradigms
+    # Dividir el código en líneas, espacios y el carácter ":"
+    lines_and_tokens = [token for line in code.split('\n') for token in line.split() + line.split(":") + line.split(".")]
 
 
-def detect_language(code):
-    try:
-        lexer = guess_lexer(code)
-        language = lexer.name
-        return language
-    except Exception as e:
-        return f"No se pudo detectar el lenguaje: {str(e)}"
+    # Contadores para diferentes paradigmas
+    count_imperative = 0
+    count_object_oriented = 0
+    count_functional = 0
 
+    # Diccionario de palabras clave y características de cada paradigma
+    paradigms_keywords = {
+        'Imperativo': ['for', 'while', 'if', 'else', 'switch', 'break', 'continue'],
+        'Orientado a Objetos': ['class', 'def', 'new', 'this', 'super', 'private', 'public', 'protected', 'extends'],
+        'Funcional': ['lambda', 'map', 'reduce', 'filter']
+    }
+
+    # Analizar los tokens del código
+    for token in lines_and_tokens:
+        # Detección de características de programación en el lenguaje detectado
+        for paradigm, keywords in paradigms_keywords.items():
+            for keyword in keywords:
+                if keyword == token:
+                    print(f"Se encontró una similitud con el paradigma {paradigm}: {token}")
+
+                    if paradigm == 'Imperativo':
+                        count_imperative += 1
+                    elif paradigm == 'Orientado a Objetos':
+                        count_object_oriented += 1
+                    elif paradigm == 'Funcional':
+                        count_functional += 1
+
+    # Determinar el paradigma predominante
+    paradigms = {
+        'Imperativo': count_imperative,
+        'Orientado a Objetos': count_object_oriented,
+        'Funcional': count_functional
+    }
+
+    predominant_paradigm = max(paradigms, key=paradigms.get)
+    print("Imperativo " + str(count_imperative))
+    print("Funcional " + str(count_functional))
+    print("Poo " + str(count_object_oriented))
+
+    return predominant_paradigm
 
 # Ejemplo de código para detectar el paradigma
 sample_code = """
+using Money
+using Money.CurrencyLayer
 
-require 'money'
-require 'money/bank/currencylayer_bank'
-
-# Configurar el conversor de divisas con la API de currencylayer (requiere una clave API válida)
-Money::Rails.configure do |config|
-  config.default_bank = Money::Bank::Currencylayer.new
-  config.default_currency = 'USD'
-  config.locale_backend = :i18n
+# Configurar el conversor de divisas con la API de CurrencyLayer (requiere una clave API válida)
+Money.Rails.configure do config
+    config.default_bank = Money.Bank.CurrencyLayer()
+    config.default_currency = "USD"
+    config.locale_backend = :i18n
 end
 
 # Mostrar las monedas disponibles para conversión
-currencies = Money::Currency.table.keys
-puts "Monedas disponibles para conversión:"
-currencies.each { |currency| puts currency }
-
-# Solicitar al usuario ingresar la moneda de origen y destino
-print "Ingresa la moneda de origen (ejemplo: USD): "
-from_currency = gets.chomp.upcase
-
-print "Ingresa la moneda de destino (ejemplo: EUR): "
-to_currency = gets.chomp.upcase
-
-# Verificar si las monedas ingresadas son válidas
-if currencies.include?(from_currency) && currencies.include?(to_currency)
-  print "Ingresa la cantidad en #{from_currency}: "
-  amount = gets.chomp.to_f
-
-  # Realizar la conversión de divisas
-  money = Money.new(amount * 100, from_currency)
-  converted_money = money.exchange_to(to_currency)
-  converted_amount = converted_money.to_f / 100
-
-  puts "#{amount} #{from_currency} es igual a #{converted_amount} #{to_currency}"
-else
-  puts "Moneda de origen o destino no válida. Asegúrate de usar códigos de moneda válidos."
+currencies = keys(Money.Currency.table)
+println("Monedas disponibles para conversión:")
+for currency in currencies
+    println(currency)
 end
 
-
-"""
-
-result = detect_paradigm(sample_code)
-print(f"El paradigma predominante es: {result}")
-
-language_detected = detect_language(sample_code)
-print(f"El lenguaje detectado es: {language_detected}")
-
-
-"""
-from forex_python.converter import CurrencyRates
-
-# Crear una instancia de CurrencyRates
-c = CurrencyRates()
-
-# Mostrar las monedas disponibles para conversión
-print("Monedas disponibles para conversión:")
-currencies = c.get_rates("")
-for currency in currencies:
-    print(currency)
-
 # Solicitar al usuario ingresar la moneda de origen y destino
-from_currency = input("Ingresa la moneda de origen (ejemplo: USD): ").upper()
-to_currency = input("Ingresa la moneda de destino (ejemplo: EUR): ").upper()
+println("Ingresa la moneda de origen (ejemplo: USD): ")
+from_currency = uppercase(strip(readline()))
+
+println("Ingresa la moneda de destino (ejemplo: EUR): ")
+to_currency = uppercase(strip(readline()))
 
 # Verificar si las monedas ingresadas son válidas
-if from_currency in currencies and to_currency in currencies:
-    amount = float(input(f"Ingresa la cantidad en {from_currency}: "))
+if from_currency in keys(Money.Currency.table) && to_currency in keys(Money.Currency.table)
+    println("Ingresa la cantidad en $from_currency: ")
+    amount = parse(Float64, strip(readline()))
 
     # Realizar la conversión de divisas
-    converted_amount = c.convert(from_currency, to_currency, amount)
+    money = Money(amount * 100, from_currency)
+    converted_money = exchange_to(money, to_currency)
+    converted_amount = to_float(converted_money) / 100
 
-    print(f"{amount} {from_currency} es igual a {converted_amount} {to_currency}")
-else:
-    print("Moneda de origen o destino no válida. Asegúrate de usar códigos de moneda válidos.")
+    println("$amount $from_currency es igual a $converted_amount $to_currency")
+else
+    println("Moneda de origen o destino no válida. Asegúrate de usar códigos de moneda válidos.")
+end
+
 """
+
+paradigm=detect_paradigm(sample_code)
+print(f"El paradigma predominante es: {paradigm}")
